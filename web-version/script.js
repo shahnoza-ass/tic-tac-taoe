b1a5957cb4560b06efcc79f02775bb933c1bd191
shahnoza <shaxnozanutfilloyeva@gmail.com>
2024-12-03 09:56:58 +0200

    Update script.js

--- web-version/script.js
+++ web-version/script.js
@@ -1,236 +1,74 @@
-/*Code of Minmax here*/
+let board = ['', '', '', '', '', '', '', '', '']; 
+let currentPlayer = 'X'; 
+let gameOver = false;
 
-var board = [
-	[0, 0, 0],
-	[0, 0, 0],
-	[0, 0, 0],
-];
+const boardElement = document.getElementById('board');
+const messageElement = document.getElementById('message');
 
-var HUMAN = -1;
-var COMP = +1;
-
-/* Function to heuristic evaluation of state. */
-function evalute(state) {
-	var score = 0;
-
-	if (gameOver(state, COMP)) {
-		score = +1;
-	}
-	else if (gameOver(state, HUMAN)) {
-		score = -1;
-	} else {
-		score = 0;
-	}
-
-	return score;
+function createBoard() {
+    boardElement.innerHTML = ''; 
+    for (let i = 0; i < 9; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('click', () => makeMove(i));
+        boardElement.appendChild(square);
+    }
 }
 
-/* This function tests if a specific player wins */
-function gameOver(state, player) {
-	var win_state = [
-		[state[0][0], state[0][1], state[0][2]],
-		[state[1][0], state[1][1], state[1][2]],
-		[state[2][0], state[2][1], state[2][2]],
-		[state[0][0], state[1][0], state[2][0]],
-		[state[0][1], state[1][1], state[2][1]],
-		[state[0][2], state[1][2], state[2][2]],
-		[state[0][0], state[1][1], state[2][2]],
-		[state[2][0], state[1][1], state[0][2]],
-	];
-
-	for (var i = 0; i < 8; i++) {
-		var line = win_state[i];
-		var filled = 0;
-		for (var j = 0; j < 3; j++) {
-			if (line[j] == player)
-				filled++;
-		}
-		if (filled == 3)
-			return true;
-	}
-	return false;
+function updateBoard() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square, index) => {
+        square.innerHTML = ''; // Eski rasmni tozalash
+        if (board[index] === 'X') {
+            square.textContent = 'X'; // X belgisi matn sifatida
+            square.classList.add('x'); // X rangini qo‘shish
+            square.classList.remove('o'); // O rangini olib tashlash
+        } else if (board[index] === 'O') {
+            square.textContent = 'O'; // O belgisi matn sifatida
+            square.classList.add('o'); // O rangini qo‘shish
+            square.classList.remove('x'); // X rangini olib tashlash
+        }
+    });
+}
+function makeMove(index) {
+    if (board[index] === '' && !gameOver) {
+        board[index] = currentPlayer;
+        updateBoard();
+        if (checkWinner()) {
+            gameOver = true;
+            messageElement.textContent = ${currentPlayer} wins!;
+        } else if (board.every(cell => cell !== '')) {
+            gameOver = true;
+            messageElement.textContent = "It's a draw!";
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
 }
 
-/* This function test if the human or computer wins */
-function gameOverAll(state) {
-	return gameOver(state, HUMAN) || gameOver(state, COMP);
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (let combo of winningCombinations) {
+        const [a, b, c] = combo;
+        if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-function emptyCells(state) {
-	var cells = [];
-	for (var x = 0; x < 3; x++) {
-		for (var y = 0; y < 3; y++) {
-			if (state[x][y] == 0)
-				cells.push([x, y]);
-		}
-	}
-
-	return cells;
+function resetGame() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    gameOver = false;
+    messageElement.textContent = '';
+    updateBoard();
 }
 
-/* A move is valid if the chosen cell is empty */
-function validMove(x, y) {
-	var empties = emptyCells(board);
-	try {
-		if (board[x][y] == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	} catch (e) {
-		return false;
-	}
-}
-
-/* Set the move on board, if the coordinates are valid */
-function setMove(x, y, player) {
-	if (validMove(x, y)) {
-		board[x][y] = player;
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-/* *** AI function that choice the best move *** */
-// Read more on https://github.com/Cledersonbc/tic-tac-toe-minimax/
-function minimax(state, depth, player) {
-	var best;
-
-	if (player == COMP) {
-		best = [-1, -1, -1000];
-	}
-	else {
-		best = [-1, -1, +1000];
-	}
-
-	if (depth == 0 || gameOverAll(state)) {
-		var score = evalute(state);
-		return [-1, -1, score];
-	}
-
-	emptyCells(state).forEach(function (cell) {
-		var x = cell[0];
-		var y = cell[1];
-		state[x][y] = player;
-		var score = minimax(state, depth - 1, -player);
-		state[x][y] = 0;
-		score[0] = x;
-		score[1] = y;
-
-		if (player == COMP) {
-			if (score[2] > best[2])
-				best = score;
-		}
-		else {
-			if (score[2] < best[2])
-				best = score;
-		}
-	});
-
-	return best;
-}
-
-/* It calls the minimax function */
-function aiTurn() {
-	var x, y;
-	var move;
-	var cell;
-
-	if (emptyCells(board).length == 9) {
-		x = parseInt(Math.random() * 3);
-		y = parseInt(Math.random() * 3);
-	}
-	else {
-		move = minimax(board, emptyCells(board).length, COMP);
-		x = move[0];
-		y = move[1];
-	}
-
-	if (setMove(x, y, COMP)) {
-		cell = document.getElementById(String(x) + String(y));
-		cell.innerHTML = "O";
-	}
-}
-
-/* main */
-function clickedCell(cell) {
-	var button = document.getElementById("bnt-restart");
-	button.disabled = true;
-	var conditionToContinue = gameOverAll(board) == false && emptyCells(board).length > 0;
-
-	if (conditionToContinue == true) {
-		var x = cell.id.split("")[0];
-		var y = cell.id.split("")[1];
-		var move = setMove(x, y, HUMAN);
-		if (move == true) {
-			cell.innerHTML = "X";
-			if (conditionToContinue)
-				aiTurn();
-		}
-	}
-	if (gameOver(board, COMP)) {
-		var lines;
-		var cell;
-		var msg;
-
-		if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
-			lines = [[0, 0], [0, 1], [0, 2]];
-		else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1)
-			lines = [[1, 0], [1, 1], [1, 2]];
-		else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1)
-			lines = [[2, 0], [2, 1], [2, 2]];
-		else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1)
-			lines = [[0, 0], [1, 0], [2, 0]];
-		else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1)
-			lines = [[0, 1], [1, 1], [2, 1]];
-		else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1)
-			lines = [[0, 2], [1, 2], [2, 2]];
-		else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1)
-			lines = [[0, 0], [1, 1], [2, 2]];
-		else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1)
-			lines = [[2, 0], [1, 1], [0, 2]];
-
-		for (var i = 0; i < lines.length; i++) {
-			cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]));
-			cell.style.color = "red";
-		}
-
-		msg = document.getElementById("message");
-		msg.innerHTML = "You lose!";
-	}
-	if (emptyCells(board).length == 0 && !gameOverAll(board)) {
-		var msg = document.getElementById("message");
-		msg.innerHTML = "Draw!";
-	}
-	if (gameOverAll(board) == true || emptyCells(board).length == 0) {
-		button.value = "Restart";
-		button.disabled = false;
-	}
-}
-
-/* Restart the game*/
-function restartBnt(button) {
-	if (button.value == "Start AI") {
-		aiTurn();
-		button.disabled = true;
-	}
-	else if (button.value == "Restart") {
-		var htmlBoard;
-		var msg;
-
-		for (var x = 0; x < 3; x++) {
-			for (var y = 0; y < 3; y++) {
-				board[x][y] = 0;
-				htmlBoard = document.getElementById(String(x) + String(y));
-				htmlBoard.style.color = "#444";
-				htmlBoard.innerHTML = "";
-			}
-		}
-		button.value = "Start AI";
-		msg = document.getElementById("message");
-		msg.innerHTML = "";
-	}
-}
+createBoard();
+updateBoard();
